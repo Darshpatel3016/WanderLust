8c4a47e27fd3a2eee6d7c2f3d9970795bbd0b6a1
Darshpatel3016 <darshpatel3016@gmail.com>
2025-02-18 22:36:57 +0530

    5

--- controllers/users.js
+++ controllers/users.js
@@ -1,63 +1,60 @@
 const User = require("../models/user");
 
 module.exports.home = (req, res) => {
-    res.send("<h1>Welcome to Wanderlust</h1>");
-    // try {
-    //     console.log("Rendering home.ejs..")
-    //     res.render("home");
-    // } catch (err) {
+  res.send("home.ejs");
+  // try {
+  //     console.log("Rendering home.ejs..")
+  //     res.render("home");
+  // } catch (err) {
 
-    //     console.error("Error rendering home.ejs", err);
-    //     res.status(500).send("Internal Server Error");
+  //     console.error("Error rendering home.ejs", err);
+  //     res.status(500).send("Internal Server Error");
 
-    //     // req.flash("error", e.message);
-    //     // res.redirect("/listings");
-    // }
-}
+  //     // req.flash("error", e.message);
+  //     // res.redirect("/listings");
+  // }
+};
 
 module.exports.renderSignupForm = (req, res) => {
-    res.render("users/signup.ejs");
+  res.render("users/signup.ejs");
 };
 
 module.exports.signup = async (req, res) => {
-    try {
-        let { username, email, password } = req.body;
-        const newUser = new User({ email, username });
-        const registeredUser = await User.register(newUser, password);
-        console.log(registeredUser);
-        req.login(registeredUser, (err) => {
-
-            if (err) {
-                return next(err);
-            }
-            req.flash("success", "Welcome to Wanderlust");
-            res.redirect("/listings");
-        });
-
-    } catch (e) {
-        req.flash("error", e.message);
-        res.redirect("/signup");
-    }
+  try {
+    let { username, email, password } = req.body;
+    const newUser = new User({ email, username });
+    const registeredUser = await User.register(newUser, password);
+    console.log(registeredUser);
+    req.login(registeredUser, (err) => {
+      if (err) {
+        return next(err);
+      }
+      req.flash("success", "Welcome to Wanderlust");
+      res.redirect("/listings");
+    });
+  } catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+  }
 };
 
 module.exports.renderLoginForm = (req, res) => {
-    res.render("users/login.ejs");
+  res.render("users/login.ejs");
 };
 
 module.exports.login = async (req, res) => {
-    req.flash("success", "Welcome back to Wanderlust...!");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
+  req.flash("success", "Welcome back to Wanderlust...!");
+  let redirectUrl = res.locals.redirectUrl || "/listings";
+  res.redirect(redirectUrl);
 };
 
 module.exports.logout = (req, res, next) => {
-    req.logout((err) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
 
-        if (err) {
-            return next(err);
-        }
-
-        req.flash("success", "You are logged out..!");
-        res.redirect("/listings");
-    });
+    req.flash("success", "You are logged out..!");
+    res.redirect("/listings");
+  });
 };
